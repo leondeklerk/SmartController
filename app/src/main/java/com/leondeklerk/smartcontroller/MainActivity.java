4c9698835d700f90ab4559fc6995ca734b816cb5
Leon <developer@leondeklerk.com>
2020-02-19 20:25:21 +0100

    Add unknown sate Add visibility switch for the colorDots Set default state

--- app/src/main/java/com/leondeklerk/smartcontroller/MainActivity.java
+++ app/src/main/java/com/leondeklerk/smartcontroller/MainActivity.java
@@ -2,13 +2,14 @@ package com.leondeklerk.smartcontroller;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatDelegate;
+
 import com.leondeklerk.smartcontroller.data.Response;
 import com.leondeklerk.smartcontroller.widget.ColorDotView;
 import org.json.JSONException;
@@ -24,6 +25,10 @@ public class MainActivity extends AppCompatActivity implements NetworkCallback {
     setContentView(R.layout.activity_main);
     context = this;
 //    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    ColorDotView colorDotView = findViewById(R.id.statusLed);
+    setVisibility(colorDotView, false);
+    TextView statusView = findViewById(R.id.deviceStatus);
+    statusView.setText(getString(R.string.device_status, getString(R.string.status_unknown)));
     TextView ip = findViewById(R.id.deviceIp);
     ip.setText(getString(R.string.device_ip, "192.168.1.217"));
     NetworkTask status = new NetworkTask((NetworkCallback) context);
@@ -41,9 +46,14 @@ public class MainActivity extends AppCompatActivity implements NetworkCallback {
 
   @Override
   public void onFinish(Response response) {
+    ColorDotView colorDotView = findViewById(R.id.statusLed);
+    TextView status = findViewById(R.id.deviceStatus);
     if (response.getException() != null) {
-      Toast.makeText(this, response.getException().toString(), Toast.LENGTH_LONG).show();
+      Log.d("Network error", response.getException().toString());
+      status.setText(getString(R.string.device_status, getString(R.string.status_unknown)));
+      setVisibility(colorDotView, false);
     } else {
+      setVisibility(colorDotView, true);
       String statusString = null;
       try {
         JSONObject obj = new JSONObject(response.getResponse());
@@ -51,15 +61,21 @@ public class MainActivity extends AppCompatActivity implements NetworkCallback {
       } catch (JSONException e) {
         e.printStackTrace();
       }
-      ColorDotView colorDotView = findViewById(R.id.statusLed);
       if (statusString.equals("ON")) {
         colorDotView.setFillColor(getColor(R.color.status_on));
       } else {
         colorDotView.setFillColor(getColor(R.color.status_off));
       }
-      TextView status = findViewById(R.id.deviceStatus);
       status.setText(getString(R.string.device_status, statusString));
       Toast.makeText(this, response.getResponse(), Toast.LENGTH_LONG).show();
+    }
+  }
+
+  public void setVisibility(View view, boolean on){
+    if(on) {
+      view.setVisibility(View.VISIBLE);
+    } else {
+      view.setVisibility(View.INVISIBLE);
     }
   }
 }
